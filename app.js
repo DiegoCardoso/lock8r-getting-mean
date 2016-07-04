98f8ac2152577b2bc6d1559167644346bc1aab52
Diego Cardoso <diegopc86@gmail.com>
2016-07-04 11:28:41 -0300

    Initial client setup in Angular JS

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_client')));
 
 app.use('/', routes);
 app.use('/api', routesApi);
